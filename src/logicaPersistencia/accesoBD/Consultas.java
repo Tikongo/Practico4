a9e22dfb66791b0a503bf2ea24942a89ac73c426
rggit1981 <rgordano@gmail.com>
2018-10-29 11:09:23 -0300

    Update src/logicaPersistencia/accesoBD/Consultas.java
    Agregado Consultas

--- src/logicaPersistencia/accesoBD/Consultas.java
+++ src/logicaPersistencia/accesoBD/Consultas.java
@@ -3,37 +3,37 @@ package logicaPersistencia.accesoBD;
 public class Consultas {
 
 	public String queryAgregarFolio() {
-		String query = "";
+		String query = "INSERT INTO Folios (codigo,caratula,pagina) values (?,?,?)";
 		return query;
 	}
 	
 	public String queryAgregarRevision() {
-		String query = "";
+		String query = "INSERT INTO Revisiones (numero,codigoFolio,descripcion) values (?,?,?)";
 		return query;
 	}
 	
 	public String queryBorrarFolioRevisiones() {
-		String query = "";
+		String query = "DELETE FROM Revisiones WHERE codigoFolio= ? ";
 		return query;
 	}
 	
 	public String queryDarDescripcion() {
-		String query = "";
+		String query = "SELECT descripcion FROM Revisiones where codigoFolio= ? AND numero = ?";
 		return query;
 	}
 	
 	public String queryListarFolios() {
-		String query = "";
+		String query = "SELECT * FROM Folios ORDER BY codigo";
 		return query;
 	}
 	
 	public String queryListarRevisiones() {
-		String query = "";
+		String query = "SELECT * FROM Revisiones where codigoFolio = ?  ORDER BY numero";
 		return query;
 	}
 	
 	public String queryFolioMasRevisado() {
-		String query = "";
+		String query = "SELECT * FROM Folios f,Revisiones r where f.codigo=r.codigoFolio GROUP BY r.numero ORDER BY COUNT (r.numero) DESC LIMIT 1 ";
 		return query;
 	}
 }
