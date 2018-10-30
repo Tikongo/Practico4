b15f38a698061f3cd5fd0b9e0f2b0e541311d08e
rggit1981 <rgordano@gmail.com>
2018-10-30 13:11:28 -0300

    Update src/persistencia/consultas/Consultas.java
    consultas de persistencia

---
+++ src/persistencia/consultas/Consultas.java
@@ -0,0 +1,39 @@
+package persistencia.consultas;
+
+public class Consultas {
+
+	public String queryAgregarFolio() {
+		String query = "INSERT INTO Folios (codigo,caratula,pagina) values (?,?,?)";
+		return query;
+	}
+	
+	public String queryAgregarRevision() {
+		String query = "INSERT INTO Revisiones (numero,codigoFolio,descripcion) values (?,?,?)";
+		return query;
+	}
+	
+	public String queryBorrarFolioRevisiones() {
+		String query = "DELETE FROM Revisiones WHERE codigoFolio= ? ";
+		return query;
+	}
+	
+	public String queryDarDescripcion() {
+		String query = "SELECT descripcion FROM Revisiones where codigoFolio= ? AND numero = ?";
+		return query;
+	}
+	
+	public String queryListarFolios() {
+		String query = "SELECT * FROM Folios ORDER BY codigo";
+		return query;
+	}
+	
+	public String queryListarRevisiones() {
+		String query = "SELECT * FROM Revisiones where codigoFolio = ?  ORDER BY numero";
+		return query;
+	}
+	
+	public String queryFolioMasRevisado() {
+		String query = "SELECT * FROM Folios f,Revisiones r where f.codigo=r.codigoFolio GROUP BY r.numero ORDER BY COUNT (r.numero) DESC LIMIT 1 ";
+		return query;
+	}
+}
