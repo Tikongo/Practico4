bcfcf0273c2b51638cf2d1e365289c47c07c96a8
rggit1981 <rgordano@gmail.com>
2018-10-30 14:36:23 -0300

    Update src/persistencia/daos/DAOFolios.java
    cabezales DAOFOlios

--- src/persistencia/daos/DAOFolios.java
+++ src/persistencia/daos/DAOFolios.java
@@ -1,5 +1,54 @@
 package persistencia.daos;
 
-public class DAOFolios {
+import java.util.ArrayList;
+import java.util.List;
 
+import logicaPersistencia.Folio;
+import logicaPersistencia.valueObjects.VOFolio;
+import logicaPersistencia.valueObjects.VOFolioMaxRev;
+import logicaPersistencia.valueObjects.VORevision;
+
+public class DAOFolios {
+	
+	// atributos para acceso a BD
+	
+	public DAOFolios() {
+		
+		
+	}
+	
+	public boolean member(String cod) {
+		boolean esta=false;
+		return esta;
+	} 
+
+	public void insert(Folio fol) {
+		
+	}
+	
+	public Folio find(String cod) {
+		Folio folio=null;
+		return folio;
+	}
+	
+	public void delete (String cod) {
+		
+	}
+	
+	public List<VOFolio> listarFolios(){
+		
+		List<VOFolio> listaFolios = new ArrayList<>();
+		return listaFolios;
+	}
+	
+	public boolean esVacio() {
+		boolean vacio=false;
+		return vacio;
+	}
+	
+	public VOFolioMaxRev folioMasRevisado() {
+		VOFolioMaxRev fmr=null;
+		return fmr;
+	}
+	
 }
