5d9512296df0fed94c9888dc1f2990d5c4e29dcb
rggit1981 <rgordano@gmail.com>
2018-11-02 14:41:03 -0300

    Update src/logicaPersistencia/accesoBD/IPoolConexiones.java

--- src/logicaPersistencia/accesoBD/IPoolConexiones.java
+++ src/logicaPersistencia/accesoBD/IPoolConexiones.java
@@ -1,5 +1,11 @@
 package logicaPersistencia.accesoBD;
 
+import java.sql.SQLException;
+
 public interface IPoolConexiones {
+	
+	public IConexion obtenerConexion(Boolean t) throws SQLException;
+	
+	public void liberarConexion(IConexion iC, Boolean t);
 	
 }
