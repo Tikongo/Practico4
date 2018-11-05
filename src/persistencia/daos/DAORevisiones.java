648003a6db755b951afece066446fdc3371584e0
rggit1981 <rgordano@gmail.com>
2018-11-05 11:28:55 -0300

    Update src/persistencia/daos/DAORevisiones.java
    metodos DAORevisiones

--- src/persistencia/daos/DAORevisiones.java
+++ src/persistencia/daos/DAORevisiones.java
@@ -1,9 +1,17 @@
 package persistencia.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import logicaPersistencia.Revision;
+import logicaPersistencia.accesoBD.Conexion;
+import logicaPersistencia.accesoBD.Consultas;
+import logicaPersistencia.accesoBD.IConexion;
+import logicaPersistencia.excepciones.ExcepPersistencia;
 import logicaPersistencia.valueObjects.VOFolio;
 import logicaPersistencia.valueObjects.VORevision;
 
@@ -18,28 +26,60 @@ public class DAORevisiones {
 		this.codigoFolio = codigoFolio;
 	}
 	
-	public void insBack (Revision rev) {
+	public void insBack (IConexion icon,Revision rev) throws ExcepPersistencia{
 		
 	}
 	
-	public int largo () {
+	public int largo (IConexion icon) throws ExcepPersistencia{
 		int largo=0;
 		return largo;
 	}
 
-	public Revision kesimo(int numero){
+	public Revision kesimo(IConexion icon,int numero) throws ExcepPersistencia{
 		Revision rev=null;
 		return rev;
 		
 	}
 	
-    public List<VORevision> listarRevisiones(){
+    public List<VORevision> listarRevisiones(IConexion icon) throws ExcepPersistencia{
 		
 		List<VORevision> listaRevisiones = new ArrayList<>();
+		try {
+			Consultas consultas = new Consultas();
+			Connection con= ((Conexion) icon).getConexion();
+			String listarRevisiones = consultas.queryListarRevisiones();
+			PreparedStatement pstmt = con.prepareStatement(listarRevisiones);
+			pstmt.setString(1, codigoFolio);		
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int numero = rs.getInt("numero");
+				String codigoF = rs.getString("codigoFolio");
+				String desc = rs.getString("descripcion");
+				VORevision revision = new VORevision(numero,desc,codigoF);
+				listaRevisiones.add(revision);
+			}
+			rs.close();		
+			pstmt.close();
+			
+		}catch (SQLException e) {
+			throw new ExcepPersistencia("Error de conexion");
+		}
 		return listaRevisiones;
 	}
     
-    public void borrarRevisiones () {
-    	
+    public void borrarRevisiones (IConexion icon) throws ExcepPersistencia{
+    	try {
+			Consultas consultas = new Consultas();
+			Connection con= ((Conexion) icon).getConexion();
+			String delete = consultas.queryBorrarFolioRevisiones();
+			PreparedStatement pstmt = con.prepareStatement(delete);
+			pstmt.setString(1, codigoFolio);
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+		}catch (SQLException e) {
+			throw new ExcepPersistencia("Error de conexion");
+		}
     }
 }
